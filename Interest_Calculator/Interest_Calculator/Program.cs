fbf4a6e45325b31c4a722d9c62a9ef0db9661fc0
shivamgoel1614 <shivamgoel1008@gmail.com>
2023-07-02 11:40:30 +0530

    Interest Calculator

---
+++ Interest_Calculator/Interest_Calculator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interest_Calculator
+{
+    internal class Program
+    {
+        public static decimal CalculateBalance(decimal deposit, decimal interestRate, int numberOfYears)
+        {
+            decimal balance = 0;
+            decimal interest = 0;
+
+            for (int year = 1; year <= numberOfYears; year++)
+            {
+                interest = (deposit + balance) * (interestRate / 100);
+                balance = balance + deposit + interest;
+            }
+
+            return balance;
+        }
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter the annual deposit: ");
+            decimal annualDeposit = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Enter the interest rate (%): ");
+            decimal interestRate = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Enter the number of years: ");
+            int numberOfYears = int.Parse(Console.ReadLine());
+
+            decimal finalBalance = CalculateBalance(annualDeposit, interestRate, numberOfYears);
+            Console.WriteLine($"Final balance after {numberOfYears} years: €{finalBalance:F2}");
+            Console.ReadLine();
+
+        }
+    }
+}
